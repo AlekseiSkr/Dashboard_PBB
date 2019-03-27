44a3d67bf69a4037340eb6f84d594e0db157615a
Chernov <chernovd2411@gmail.com>
2019-03-27 20:52:59 +0100

    Update table.php

--- table.php
+++ table.php
@@ -8,13 +8,13 @@
                 <?php
 
                         include_once('dbh-inc.php');
-<<<<<<< HEAD
+
                         $SQLstring = "SELECT Team,ObstacleRace,Sumo,FollowTheLine,Football FROM Scoreboard";
                         if ($stmt = mysqli_prepare($conn, $SQLstring)) 
-=======
-                        $SQLstring = "SELECT Team,ObstacleRace,Sumo,Football FROM Scoreboard";
+
+                        $SQLstring = "SELECT Team,ObstacleRace,Sumo,FollowTheLine,Football FROM Scoreboard";
                         if ($stmt = mysqli_prepare($conn, $SQLstring))
->>>>>>> 491b72626fc73b82addcd4ac0fb421644d7b0a86
+
                         {
                             mysqli_stmt_execute($stmt) OR DIE("Error!");
                             mysqli_stmt_bind_result($stmt, $team,$race,$sumo,$line,$football);
@@ -29,26 +29,23 @@
                             else
                             {
 
-<<<<<<< HEAD
+
                                 
                                 
-                                    echo "<table>
-                                    <tr>
-                                    <th>Team </th>
-                                    <th>Obstacle Race </th> 
-=======
+                                   
+
                                     echo "<div id='scoreboard-container'>";
                                     echo "<h1>Scoreboard</h1>";
-                                    $table = "<table><tr>
+                                    echo "<table><tr>
                                     <th>Teams</th>
                                     <th>Obstacle Race</th>
->>>>>>> 491b72626fc73b82addcd4ac0fb421644d7b0a86
+
                                     <th>Sumo</th>
                                     <th>Follow the line </th>
                                     <th>Football</th>
                                   </tr>";
                                 while (mysqli_stmt_fetch($stmt)) {
-<<<<<<< HEAD
+
                                 echo "<tr><td>".$team."</td>";
                                 echo "<td>".$race."</td>";
                                 echo "<td>".$sumo."</td>";
@@ -58,16 +55,9 @@
                                 ";
                                 }
                                 echo "</table>";
-=======
-                                $table .= "<tr><td>".$team."</td>"
-                                .  "<td>".$race."</td>"
-                                .  "<td>".$sumo."</td>"
-                                .  "<td>".$football."</td></tr>";
-                                }
-                                $table .= "</table>";
-                                echo $table;
                                 echo "</div>";
->>>>>>> 491b72626fc73b82addcd4ac0fb421644d7b0a86
+                               
+
                             }
                                  mysqli_stmt_close($stmt);
                         }

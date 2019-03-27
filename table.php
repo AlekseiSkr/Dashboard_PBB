08ee2cbd521526e24fe0e6b59c4b1c11a79eba24
Chernov <chernovd2411@gmail.com>
2019-03-27 20:45:28 +0100

    Update table.php

--- table.php
+++ table.php
@@ -8,11 +8,11 @@
                 <?php
                 
                         include_once('dbh-inc.php');
-                        $SQLstring = "SELECT Team,ObstacleRace,Sumo,Football FROM Scoreboard";
+                        $SQLstring = "SELECT Team,ObstacleRace,Sumo,FollowTheLine,Football FROM Scoreboard";
                         if ($stmt = mysqli_prepare($conn, $SQLstring)) 
                         {
                             mysqli_stmt_execute($stmt) OR DIE("Error!");
-                            mysqli_stmt_bind_result($stmt, $team,$race,$sumo,$football);
+                            mysqli_stmt_bind_result($stmt, $team,$race,$sumo,$line,$football);
                             mysqli_stmt_store_result($stmt);
 
                             if (mysqli_stmt_num_rows($stmt) == 0) 
@@ -26,20 +26,24 @@
 
                                 
                                 
-                                    echo "<tr>
-                                    <th>Teams</th>
-                                    <th>Obstacle Race</th> 
+                                    echo "<table>
+                                    <tr>
+                                    <th>Team </th>
+                                    <th>Obstacle Race </th> 
                                     <th>Sumo</th>
-                                    <th>Follow the line</th>
+                                    <th>Follow the line </th>
                                     <th>Football</th>
                                   </tr>";
                                 while (mysqli_stmt_fetch($stmt)) {
                                 echo "<tr><td>".$team."</td>";
                                 echo "<td>".$race."</td>";
                                 echo "<td>".$sumo."</td>";
-                                echo "<td>".$football."</td></tr>";
+                                echo "<td>".$line."</td>";
+                                echo "<td>".$football."</td>
+                                </tr>
+                                ";
                                 }
-
+                                echo "</table>";
                             }
                                  mysqli_stmt_close($stmt);
                         }
